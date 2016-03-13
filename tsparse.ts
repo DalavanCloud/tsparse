5575331d037c12baa5c79d3ca3a5b1212b39fd2d
Evan Martin <martine@danga.com>
2016-03-13 12:04:55 -0700

    output file

--- tsparse.ts
+++ tsparse.ts
@@ -8,11 +8,18 @@ import * as path from 'path';
 let parser = new ArgumentParser({
     addHelp: true,
 });
+parser.addArgument(['-o', '--out'], {
+    help: 'output path',
+});
 parser.addArgument(['infile'], {
     help: 'input file',
 });
 let args = parser.parseArgs();
 
+let outfile = process.stdout;
+if (args.out) {
+    outfile = fs.createWriteStream(args.out);
+}
 let sourceText = fs.readFileSync(args.infile, 'utf8');
 let sf = ts.createSourceFile(args.infile, sourceText, ts.ScriptTarget.ES5, true);
 
@@ -52,11 +59,14 @@ let data: Data = {
     spans: gatherSpans(sf),
 };
 
-console.log(readDataFile('vis.html'));
-console.log(`<style>${readDataFile('style.css')}</style>`);
-console.log('<script>');
-console.log('(function(data) {');
-console.log(readDataFile('tsvis.js'));
-let json = JSON.stringify(data, null, '\t').replace(/<\/script/g, '&lt;/script');
-console.log(`})(${json});\n`);
-console.log('</script>')
+outfile.write(readDataFile('vis.html'));
+outfile.write(`<style>${readDataFile('style.css')}</style>`);
+outfile.write('<script>');
+outfile.write('(function(data) {');
+outfile.write(readDataFile('tsvis.js'));
+let json = JSON.stringify(data).replace(/<\/script/g, '&lt;/script');
+outfile.write(`})(${json});\n`);
+outfile.write('</script>')
+if (outfile !== process.stdout) {
+    outfile.end();
+}
